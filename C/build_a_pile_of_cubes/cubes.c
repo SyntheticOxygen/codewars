98eae033a8937f443f9ded759f12f83c8ac9d334
SyntheticOxygen <vs.breider@gmail.com>
2019-10-05 18:37:32 +0200

    Initial commit with partial first challenge

---
+++ C/build_a_pile_of_cubes/cubes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int findNb(int volume) {
+        
+}
+
+int main(int argc, char*[] args) {
+        int volume = 0;
+        int cubes = 0;
+
+        printf("Please enter a volume: ")
+        scanf("%d", &volume);
+        printf("\n")
+
+        cubes = findNb(volume)
+        printf("The number of cubes is: %d", cubes)
+}
